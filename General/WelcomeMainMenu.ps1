411148d38a39ce3332686729564a5d21190f6abe
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-03 00:54:35 -0400

    Create WelcomeMainMenu.ps1 for deployment scripting
    Added a PowerShell script for GitHub-based deployment solutions with various administrative functionalities.

---
+++ General/WelcomeMainMenu.ps1
@@ -0,0 +1,19 @@
+Write-Host 
+Write-Host '                                                                              ' -BackgroundColor White                                                              
+Write-Host ' This Git Hub-based deployment scripting solution allows the user to perform: ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Domain User and Computer Administration                                ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Local PC User Administration                                           ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Install OS Mandatory System Software                                   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Install PowerShell 5.1 and 7.x Modules                                 ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Scripted Install of multiple types of software configurations          ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Import of Registry Entries                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Set-up Custom Windows 11 ONLY based Start Menu                         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Configure an OSDCloud bootable USB Stick or ISO File                   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Inject drivers/updates/features - OS mods into OS Install.wim file     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Debloat the OS                                                         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Install Client and Server OS Roles                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Download Windows Client and Server ISO files                           ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - File inventory of Local PC or a Network Attached Storage Devices       ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                              ' -BackgroundColor White
+pause
+Clear-Host
