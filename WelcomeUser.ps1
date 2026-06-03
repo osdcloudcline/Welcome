f969581355277577a8996b79bef9e01e188fdf13
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-03 00:07:42 -0400

    Refactor welcome message rendering to use Write-CenteredText

--- WelcomeUser.ps1
+++ WelcomeUser.ps1
@@ -7,12 +7,12 @@ $currentDate = Get-Date -Format "dddd, MMMM dd, yyyy"
 Clear-Host
 
 # Render the welcome message with colors
-Write-Host "=========================================" -ForegroundColor Cyan
-Write-Host "  Welcome $currentUser!  " -ForegroundColor Green -BackgroundColor Black
-Write-Host "=========================================" -ForegroundColor Cyan
-Write-Host " Today is:     $currentDate" -ForegroundColor Yellow
-Write-Host " Shell Engine: PowerShell v$($PSVersionTable.PSVersion)" -ForegroundColor Cyan
-Write-Host "=========================================`n" -ForegroundColor Cyan
+Write-CenteredText  "=========================================" -ForegroundColor Cyan
+Write-CenteredText "  Welcome $currentUser!  " -ForegroundColor Green -BackgroundColor Black
+Write-CenteredText "=========================================" -ForegroundColor Cyan
+Write-CenteredText " Today is:     $currentDate" -ForegroundColor Yellow
+Write-CenteredText " Shell Engine: PowerShell v$($PSVersionTable.PSVersion)" -ForegroundColor Cyan
+Write-CenteredText "=========================================`n" -ForegroundColor Cyan
 
 # Retrive System Information
 $SysInfo = Invoke-WebRequest("https://github.com/osdcloudcline/Welcome/raw/refs/heads/main/SystemInfo.ps1") 
