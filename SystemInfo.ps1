65a621723bc1dcc37698e8c097c40ac8ab7ec59c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-02 20:02:39 -0400

    Enhance SystemInfo.ps1 with detailed verbose output

--- SystemInfo.ps1
+++ SystemInfo.ps1
@@ -26,3 +26,38 @@
        $BIOS1 = (Get-CimInstance -ClassName Win32_BIOS).Manufacturer
        $BIOS2 = (Get-CimInstance -ClassName Win32_BIOS).SMBIOSBIOSVersion
        $CheckUEFIBoot = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI PK).bytes) -match "DO NOT TRUST|DO NOT SHIP"
+
+       Write-Host
+       Write-Host         "Running Computer Info:" -ForegroundColor Green 
+       Write-Verbose "System Hostname: $PCName" -Verbose
+       Write-Verbose "Running Operating System: $CompleteOSInfo" -Verbose  
+       Write-Verbose "Operating System: $FullOS" -Verbose 
+       Write-Verbose "Build Number: $OSBuild" -Verbose 
+       Write-Verbose "Display Version: $DisplayVersion" -Verbose
+       Write-Verbose "OS Edition: $EditionID" -Verbose 
+       Write-Verbose "MS Edge Chromium Version: $EdgeBrowser" -Verbose 
+
+       Write-Host 
+       Write-Host         "System Hardware Info:" -ForegroundColor Green 
+       Write-Verbose "System Hostname: $PCName" -Verbose
+       Write-Verbose "Processor: $CPU" -Verbose
+       Write-Verbose "System Memory: $RAM GB" -Verbose
+       Write-Verbose "Motherboard: $Baseboard1 $Baseboard2" -Verbose
+       Write-Verbose "Manufacturer: $PCManufacturer" -Verbose
+       Write-Verbose "System BIOS: $BIOS1 $BIOS2" -Verbose
+      If($CheckUEFIBoot -eq $false){
+       Write-Host "Great news - $env:computername using $Baseboard1 $Baseboard2 is NOT affected by PKFail security vulneralbility" -ForegroundColor DarkBlue -BackgroundColor White
+    }
+       ElseIf($CheckUEFIBoot -eq $true){
+       Write-Host "Bad news - $env:computername using $Baseboard1 $Baseboard2 IS affected by PKFail security vulneralbility" -ForegroundColor DarkRed -BackgroundColor White
+    }
+       
+       Write-Host 
+       Write-Host         "Network Connection Info:" -ForegroundColor Green 
+       Write-Verbose "System Hostname: $PCName" -Verbose
+       Write-Verbose "System IP Address 1: $IP1" -Verbose
+       Write-Verbose "System IP Address 2: $IP2" -Verbose
+       Write-Verbose "System IP Address 3: $IP3" -Verbose
+       Write-Verbose "Network Name: $NetName" -Verbose
+       Write-Verbose "System Gateway/DNS Server: $GatewayDNS" -Verbose
+       Write-Verbose "WAN or External IP Address: $ExternalIP" -Verbose
