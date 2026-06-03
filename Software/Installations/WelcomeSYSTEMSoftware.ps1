d5c71580acdb3297b72a06f1c4ae1b4c36434bac
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-03 01:14:51 -0400

    Create WelcomeSYSTEMSoftware.ps1 to list required software
    This script displays a list of OS mandated software to be installed, including various versions of .NET SDK, .NET Runtime, Visual C++ Redistributables, and other essential software.

---
+++ Software/Installations/WelcomeSYSTEMSoftware.ps1
@@ -0,0 +1,41 @@
+Write-Host 
+Write-Host '                                                                              ' -BackgroundColor White                                                              
+Write-Host ' This following OS Mandated SYSTEM Software will bw installed:                ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Chocolatey Package Manager                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET SDK 10.0                                                ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft ASP.NET Core 3.1                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft ASP.NET Core 5.0                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft ASP.NET Core 6.0                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft ASP.NET Core 7.0                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft ASP.NET Core 8.0                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft ASP.NET Core 9.0                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft ASP.NET Core 10.0                                            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Desktop Runtime 3.1                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Desktop Runtime 5.0                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Desktop Runtime 6.0                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Desktop Runtime 7.0                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Desktop Runtime 8.0                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Desktop Runtime 9.0                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Desktop Runtime 10.0                                    ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Runtime 2.2                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Runtime 3.1                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Runtime 7.0                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Runtime 8.0                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Runtime 9.0                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Runtime 10.0                                            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft .NET Framework 4.8.1                                         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft Visual C++ Resistributables 2005-2015+ x64                   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft Visual C++ Resistributables 2005-2015+ x86                   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft Visual C++ Resistributable 2017                              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Oracle JAVA 8.0                                                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - microsoft XNA Framework 4.0                                            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Open AL                                                                ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft DirectX 9.0                                                  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Install Client and Server OS Roles                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Windows 365 App                                                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Windows 365 App Runtime 1.0                                            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft Edge Web Browser                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Microsoft Windows Terminal                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                              ' -BackgroundColor White
+pause
+Clear-Host
